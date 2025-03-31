24c60bc6c72aac064689d45d0c8ef2451696f423
fuum <wkrh832+programing@gmail.com>
2025-03-31 11:42:10 +0900

    #9

--- src/components/FooterHeader/Header/HeaderItems/HeaderItems.tsx
+++ src/components/FooterHeader/Header/HeaderItems/HeaderItems.tsx
@@ -6,13 +6,15 @@ import { usePathname } from "next/navigation";
 
 const HeaderItems = ({ headeritems }: { headeritems: HeaderItemsType[] }) => {
     const pathname = usePathname()
-    
+
     return (
         <div>
             <div className='border-b border-gray-300 flex justify-center '>
                 <div className='flex justify-center w-4/5 mt-5  '>
                     {headeritems.map((item) => {
-                        return <div className={`pt-1 w-20 mr-2 rounded-t-sm text-center hover:bg-gray-100 ${pathname === item.url ? "bg-gray-200 border-b-4 border-b-green-500" : ''}`}key={item.url}> <Link href={item.url}  >{item.displayname}</Link></div>
+                        return <a className={`pt-1 w-20 mr-2 rounded-t-sm text-center hover:bg-gray-100 ${pathname === item.url ? "bg-gray-200 border-b-4 border-b-green-500" : ''}`} key={item.url} href={item.url}>
+                            {item.displayname}
+                        </a>
                     })}
                 </div>
             </div>
